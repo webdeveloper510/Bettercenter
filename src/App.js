d7a2fe80016be3fe0a05de17c5aba3fbb7abc74c
webdeveloper510 <developerweb6@gmail.com>
2025-03-19 16:35:40 +0530

    work components

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Home from './Component/Pages/home';
 import Signup from './Component/Pages/signup';
 import Signin from './Component/Pages/signin';
+
 function App() {
   return (
     <>
+    <header/>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,7 +19,8 @@ function App() {
           
         </Routes>
       </Router>
-    </>
+      <footer/>
+      </>
   );
 }
 
