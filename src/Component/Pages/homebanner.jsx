7fd7c690c30615ce74aad2dfdc6370c4b2b8e380
webdeveloper510 <developerweb6@gmail.com>
2025-05-01 12:23:30 +0530

    changes banner image

--- src/Component/Pages/homebanner.jsx
+++ src/Component/Pages/homebanner.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import bannerimg from '../../Assets/images/homebanner.png';
+// import bannerimg from '../../Assets/images/homebanner.png';
+import bannerimg2 from '../../Assets/images/homebanner2.png';
 import '../../Assets/css/homebanner.css';
 
 const PromoBanner = () => {
   return (
     <div className="single-image-banner text-center">
       <img
-        src={bannerimg}
+        src={bannerimg2}
         alt="Promo"
         className="img-fluid banner-img"
       />
