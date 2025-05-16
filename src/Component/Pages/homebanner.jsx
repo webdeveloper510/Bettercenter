12d48e651d33699a44e48e463929436de6a631a0
webdeveloper510 <developerweb6@gmail.com>
2025-05-16 15:20:57 +0530

    added login condtions

--- src/Component/Pages/homebanner.jsx
+++ src/Component/Pages/homebanner.jsx
@@ -1,43 +1,41 @@
 import React from 'react';
 import { Carousel, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-// import banner1 from '../../Assets/images/homebanner1.png';
+import { useNavigate } from 'react-router-dom';
 import banner1 from '../../Assets/images/homebanner.png';
 import banner2 from '../../Assets/images/homebanner2.png';
 import '../../Assets/css/homebanner.css';
 
 const PromoBanner = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    const isAccessToken = localStorage.getItem("accessToken");
+    if (isAccessToken) {
+      navigate("/aipicks");
+    } else {
+      navigate("/signin");
+    }
+  };
+
   return (
     <Carousel className="promo-slider">
       {/* Slide 1 */}
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={banner1}
-          alt="First slide"
-        />
+        <img className="d-block w-100" src={banner1} alt="First slide" />
         <Carousel.Caption className="text-center">
-          <Link to="/signin">
-            <Button variant="primary" className="banner-btn mt-3">
-              CLICK HERE
-            </Button>
-          </Link>
+          <Button variant="primary" className="banner-btn mt-3" onClick={handleClick}>
+            CLICK HERE
+          </Button>
         </Carousel.Caption>
       </Carousel.Item>
 
       {/* Slide 2 */}
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={banner2}
-          alt="Second slide"
-        />
+        <img className="d-block w-100" src={banner2} alt="Second slide" />
         <Carousel.Caption className="text-center">
-          <Link to="/signin">
-            <Button variant="primary" className="banner-btn mt-3">
-              CLICK HERE
-            </Button>
-          </Link>
+          <Button variant="primary" className="banner-btn mt-3" onClick={handleClick}>
+            CLICK HERE
+          </Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
